3ecc312120db763064a857b6e8ab3b70465adfdc
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-07-31 19:32:11 +0700

    add delete feature

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {getPassListAsyncHanlder} from '../actions/passList.js'
+import axios from 'axios'
 
 class List extends Component {
+  constructor () {
+    super()
+    this.deletePass = this.deletePass.bind(this)
+  }
 
   componentWillMount () {
     this.props.getPassListAsyncHanlder()
@@ -10,18 +15,47 @@ class List extends Component {
 
   render () {
     return (
-      <div>
-        {this.props.passListReducer.map((pass, idx) => {
-          return <tr key={idx}>
-            <td>{pass.url}</td>
-            <td>{pass.username}</td>
-            <td>{pass.password}</td>
-            <td>{pass.createdAt}</td>
-            <td>{pass.UpdatedAt}</td>
-          </tr>
-        })}
+      <div className='container columns tabel'>
+        <div className='column is-10 is-offset-1'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>URL</th>
+                <th>Username</th>
+                <th>Password</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.passListReducer.map((pass, idx) => {
+                return <tr key={idx}>
+                  <td>{pass.URL}</td>
+                  <td>{pass.username}</td>
+                  <td>{pass.password}</td>
+                  <td>{pass.createdAt.split('').splice(0, 24).join('')}</td>
+                  <td>{pass.UpdatedAt}</td>
+                  <td><button className='button'>Update</button><button className='button' onClick={() => {
+                    this.deletePass(pass.id)
+                  }}>Delete</button></td>
+                </tr>
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     )
+  }
+
+  deletePass (passId) {
+    axios.delete(`http://localhost:3000/pass/${passId}`)
+    .then((resp) => {
+      this.props.getPassListAsyncHanlder()
+    })
+    .catch((err) => {
+      console.log(err)
+    })
   }
 }
 
